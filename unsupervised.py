0c796815a51351e6e2f8aff01e67bef85da5262d
Zeynep Budak <76157512+zeynepbudak@users.noreply.github.com>
2022-07-31 05:39:20 +0300

    Create unsupervised.py

---
+++ unsupervised.py
@@ -0,0 +1,145 @@
+###############################################################
+# GÖREV 1: Veri setini okutunuz ve müşterileri segmentlerken kullanıcağınız değişkenleri seçiniz.
+###############################################################
+
+import pandas as pd
+from scipy import stats
+import datetime as dt
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import dendrogram
+from scipy.cluster.hierarchy import linkage
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import AgglomerativeClustering
+import seaborn as sns
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.set_option('display.width', 1000)
+
+
+df_ = pd.read_csv("datasets/flo_data_20K.csv")
+df = df_.copy()
+
+# tarih değişkenine çevirme
+date_columns = df.columns[df.columns.str.contains("date")]
+df[date_columns] = df[date_columns].apply(pd.to_datetime)
+
+df["last_order_date"].max() # 2021-05-30
+analysis_date = dt.datetime(2021,6,1)
+
+df["recency"] = (analysis_date - df["last_order_date"]).astype('timedelta64[D]') # en son kaç gün önce alışveriş yaptı
+df["tenure"] = (df["last_order_date"]-df["first_order_date"]).astype('timedelta64[D]')
+
+model_df = df[["order_num_total_ever_online","order_num_total_ever_offline","customer_value_total_ever_offline","customer_value_total_ever_online","recency","tenure"]]
+model_df.head()
+
+###############################################################
+# GÖREV 2: K-Means ile Müşteri Segmentasyonu
+###############################################################
+
+# 1. Değişkenleri standartlaştırınız.
+#SKEWNESS
+def check_skew(df_skew, column):
+    skew = stats.skew(df_skew[column])
+    skewtest = stats.skewtest(df_skew[column])
+    plt.title('Distribution of ' + column)
+    sns.distplot(df_skew[column],color = "g")
+    print("{}'s: Skew: {}, : {}".format(column, skew, skewtest))
+    return
+
+plt.figure(figsize=(9, 9))
+plt.subplot(6, 1, 1)
+check_skew(model_df,'order_num_total_ever_online')
+plt.subplot(6, 1, 2)
+check_skew(model_df,'order_num_total_ever_offline')
+plt.subplot(6, 1, 3)
+check_skew(model_df,'customer_value_total_ever_offline')
+plt.subplot(6, 1, 4)
+check_skew(model_df,'customer_value_total_ever_online')
+plt.subplot(6, 1, 5)
+check_skew(model_df,'recency')
+plt.subplot(6, 1, 6)
+check_skew(model_df,'tenure')
+plt.tight_layout()
+plt.savefig('before_transform.png', format='png', dpi=1000)
+plt.show()
+
+# Normal dağılımın sağlanması için Log transformation uygulanması
+model_df['order_num_total_ever_online']=np.log1p(model_df['order_num_total_ever_online'])
+model_df['order_num_total_ever_offline']=np.log1p(model_df['order_num_total_ever_offline'])
+model_df['customer_value_total_ever_offline']=np.log1p(model_df['customer_value_total_ever_offline'])
+model_df['customer_value_total_ever_online']=np.log1p(model_df['customer_value_total_ever_online'])
+model_df['recency']=np.log1p(model_df['recency'])
+model_df['tenure']=np.log1p(model_df['tenure'])
+model_df.head()
+
+# Scaling
+sc = MinMaxScaler((0, 1))
+model_scaling = sc.fit_transform(model_df)
+model_df=pd.DataFrame(model_scaling,columns=model_df.columns)
+model_df.head()
+
+
+# 2. Optimum küme sayısını belirleyiniz.
+kmeans = KMeans()
+elbow = KElbowVisualizer(kmeans, k=(2, 20))
+elbow.fit(model_df)
+elbow.show()
+
+
+# 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleyiniz.
+k_means = KMeans(n_clusters = 7, random_state= 42).fit(model_df)
+segments=k_means.labels_
+segments
+
+final_df = df[["master_id","order_num_total_ever_online","order_num_total_ever_offline","customer_value_total_ever_offline","customer_value_total_ever_online","recency","tenure"]]
+final_df["segment"] = segments
+final_df.head()
+
+
+# 4. Herbir segmenti istatistiksel olarak inceleyeniz.
+final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","max"],
+                                  "order_num_total_ever_offline":["mean","min","max"],
+                                  "customer_value_total_ever_offline":["mean","min","max"],
+                                  "customer_value_total_ever_online":["mean","min","max"],
+                                  "recency":["mean","min","max"],
+                                  "tenure":["mean","min","max","count"]})
+
+
+###############################################################
+# GÖREV 3: Hierarchical Clustering ile Müşteri Segmentasyonu
+###############################################################
+
+# 1. Görev 2'de standarlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirleyiniz.
+hc_complete = linkage(model_df, 'complete')
+
+plt.figure(figsize=(7, 5))
+plt.title("Dendrograms")
+dend = dendrogram(hc_complete,
+           truncate_mode="lastp",
+           p=10,
+           show_contracted=True,
+           leaf_font_size=10)
+plt.axhline(y=1.2, color='r', linestyle='--')
+plt.show()
+
+
+# 2. Modelinizi oluşturunuz ve müşterileriniz segmentleyiniz.
+hc = AgglomerativeClustering(n_clusters=5)
+segments = hc.fit_predict(model_df)
+
+final_df = df[["master_id","order_num_total_ever_online","order_num_total_ever_offline","customer_value_total_ever_offline","customer_value_total_ever_online","recency","tenure"]]
+final_df["segment"] = segments
+final_df.head()
+
+# 3. Herbir segmenti istatistiksel olarak inceleyeniz.
+final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","max"],
+                                  "order_num_total_ever_offline":["mean","min","max"],
+                                  "customer_value_total_ever_offline":["mean","min","max"],
+                                  "customer_value_total_ever_online":["mean","min","max"],
+                                  "recency":["mean","min","max"],
+                                  "tenure":["mean","min","max","count"]})
